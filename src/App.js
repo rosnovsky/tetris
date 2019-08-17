ab9a51d4c401da732033b04e24bff0ab1d6f25c7
Artem Rosnovsky <artem@rosnovsky.us>
2019-08-17 14:30:31 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Tetris from "./components/tetris";
+
+const App = () => (
+  <div className="App">
+    <Tetris />
+  </div>
+)
+
+export default App;
---
+++ src/components/Display.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Display = ({ gaveOver, text }) => (
+    <div>{text}</div>
+)
+
+export default Display;
+
+
