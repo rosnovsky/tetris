14c06b33692b713e0c7135f0801bac0ea8638f41
Artem Rosnovsky <artem@rosnovsky.us>
2019-08-18 15:27:20 -0700

    shapes + WIP on styling

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import {StyledCell} from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <div>Cell</div>
+    <StyledCell type={type} color={TETROMINOS['L'].color}>Cell</StyledCell>
 )
 
 export default Cell;
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,17 +6,21 @@ import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 
+import { StyledTetrisWrapper,StyledTetris } from './styles/StyledTetris';
+
 const Tetris = () => {  
     return (
-        <div>
-            <Stage stage={createStage()} />
-            <aside>
-                <Display text="Score" />
-                <Display text="Rows" />
-                <Display text="Level" />
-                <StartButton />
-            </aside>
-        </div>
+        <StyledTetrisWrapper>
+            <StyledTetris>
+                <Stage stage={createStage()} />
+                <aside>
+                    <Display text="Score" />
+                    <Display text="Rows" />
+                    <Display text="Level" />
+                    <StartButton />
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
     )
 }
 
